a92bdee92c46ccbd04c49b787b7833a06c928d65
m4a1j9 <davidovandre5@gmail.com>
2024-07-28 18:35:38 +0700

    auto-session & bufferline fix

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,24 +1,31 @@
+local close_buffer = function()
+	local tab_number = vim.api.nvim_get_current_tabpage()
+	vim.cmd([[bw]])
+	vim.cmd([[BufferLineGoToBuffer tab_number]])
+end
+
 -- Отображение вкладок
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
 
-  config = function()
-    require("bufferline").setup({
-      options = {
-        mode = "buffers",
-        themable = true,
-        indicator = {
-          style = "underline",
-        },
-        -- Отображение статуса ошибок в табах
-        diagnostics = 'nvim_lsp',
-        separator_style = 'slope',
-      },
-    })
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				themable = true,
+				indicator = {
+					style = "underline",
+				},
+				-- Отображение статуса ошибок в табах
+				diagnostics = "nvim_lsp",
+				separator_style = "slope",
+			},
+		})
 
-    vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
-    vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
-  end,
+		vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+		vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
+		vim.keymap.set("n", "<leader>w", close_buffer)
+	end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,30 +1,30 @@
 local function toggle_neo_tree()
-  local manager = require("neo-tree.sources.manager")
-  local renderer = require("neo-tree.ui.renderer")
+	local manager = require("neo-tree.sources.manager")
+	local renderer = require("neo-tree.ui.renderer")
 
-  local state = manager.get_state("filesystem")
-  local window_exists = renderer.window_exists(state)
+	local state = manager.get_state("filesystem")
+	local window_exists = renderer.window_exists(state)
 
-  -- TODO
+	-- TODO
 end
 
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function ()
-      require("neo-tree").setup({
-        window = {
-          width = 30
-        }
-      })
-    end,
-    -- Открыть-закрыть файловый менеджер
-    vim.keymap.set('n', '<A-1>', ':Neotree filesystem reveal left toggle=true<CR>', {})
-
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		local config = {
+			window = {
+				width = 30,
+			},
+		}
+		require("neo-tree").setup(config)
+	end,
+	-- Открыть-закрыть файловый менеджер
+	vim.keymap.set("n", "<A-1>", ":Neotree filesystem reveal left toggle=true<CR>", {}),
 }
