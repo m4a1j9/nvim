3b83bae649c9c44c7714b288b9f587f060776d27
m4a1j9 <davidovandre5@gmail.com>
2024-08-05 19:46:20 +0700

    hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,28 +1,38 @@
 local M = {}
 
 M.plugin = {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		M.setup()
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    M.setup()
+  end,
 }
 
 M.setup = function()
-	local config = {
-		window = {
-			width = 30,
-		},
-	}
-	require("neo-tree").setup(config)
-	-- Открыть-закрыть файловый менеджер
-	vim.keymap.set("n", "<A-1>", ":Neotree filesystem reveal left<CR>", {})
+  require("neo-tree").setup({
+    window = {
+      width = 30,
+    },
+    filesystem = {
+      filtered_items = {
+        never_show_by_pattern = {
+          ".git",
+          ".idea",
+          ".vscode",
+        },
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  })
+  -- Открыть-закрыть файловый менеджер
+  vim.keymap.set("n", "<A-1>", ":Neotree filesystem reveal left<CR>", {})
 end
 
 if not pcall(debug.getlocal, 4, 1) then
