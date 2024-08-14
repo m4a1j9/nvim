7c0e095710f0f9bc5e6055c1048c1e316afb8379
Andrey Davidov <88237046+m4a1j9@users.noreply.github.com>
2024-08-14 16:53:02 +0700

    get relative file path

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,7 @@
 -- Delete unused keymaps --
 
 -- Toggle comment
-vim.keymap.del('n', 'gcc')
+vim.keymap.del("n", "gcc")
 
 -- Set new maps --
 
@@ -12,10 +12,10 @@ local opts = {
 
 -- Move thorugh windows --
 
-vim.keymap.set('n', '<A-h>', ':wincmd h<CR>', { desc = 'Go to the left window' })
-vim.keymap.set('n', '<A-j>', ':wincmd j<CR>', { desc = 'Go to the bottom window' })
-vim.keymap.set('n', '<A-k>', ':wincmd k<CR>', { desc = 'Go to the top window' })
-vim.keymap.set('n', '<A-l>', ':wincmd l<CR>', { desc = 'Go to the right window' })
+vim.keymap.set("n", "<A-h>", ":wincmd h<CR>", { desc = "Go to the left window" })
+vim.keymap.set("n", "<A-j>", ":wincmd j<CR>", { desc = "Go to the bottom window" })
+vim.keymap.set("n", "<A-k>", ":wincmd k<CR>", { desc = "Go to the top window" })
+vim.keymap.set("n", "<A-l>", ":wincmd l<CR>", { desc = "Go to the right window" })
 
 -- dap --
 
@@ -24,30 +24,20 @@ vim.keymap.set("n", "<F9>", require("dap").step_over)
 vim.keymap.set("n", "<F10>", require("dap").step_into)
 vim.keymap.set("n", "<F11>", require("dap").step_out)
 vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
-vim.keymap.set(
-  "n",
-  "<leader>B",
-  function()
-    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-  end,
-  {
-    noremap = true,
-    silent = true,
-    desc = "Set condition breakpoint",
-  }
-)
-vim.keymap.set(
-  "n",
-  "<leader>lm",
-  function()
-    require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-  end,
-  {
-    noremap = true,
-    silent = true,
-    desc = "Set logpoint",
-  }
-)
+vim.keymap.set("n", "<leader>B", function()
+  require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, {
+  noremap = true,
+  silent = true,
+  desc = "Set condition breakpoint",
+})
+vim.keymap.set("n", "<leader>lm", function()
+  require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end, {
+  noremap = true,
+  silent = true,
+  desc = "Set logpoint",
+})
 -- vim.keymap.set(
 --   "n",
 --   "<leader>dS",
@@ -61,31 +51,39 @@ vim.keymap.set("n", "<leader>ui", require("dapui").toggle)
 -- bufferline --
 
 local close_buffer = function()
-	local bufferline = require("bufferline")
-	local buf_number = vim.api.nvim_get_current_buf()
+  local bufferline = require("bufferline")
+  local buf_number = vim.api.nvim_get_current_buf()
 
-	local is_next_valid = vim.api.nvim_buf_is_valid(buf_number - 1)
-	if is_next_valid then
-		vim.cmd("bd")
-		bufferline.move_to(buf_number - 1)
-	end
+  local is_next_valid = vim.api.nvim_buf_is_valid(buf_number - 1)
+  if is_next_valid then
+    vim.cmd("bd")
+    bufferline.move_to(buf_number - 1)
+  end
 
-	local is_prev_valid = vim.api.nvim_buf_is_valid(buf_number + 1)
-	if is_prev_valid then
-		vim.cmd("bd")
-		bufferline.move_to(buf_number + 1)
-	end
+  local is_prev_valid = vim.api.nvim_buf_is_valid(buf_number + 1)
+  if is_prev_valid then
+    vim.cmd("bd")
+    bufferline.move_to(buf_number + 1)
+  end
 end
 
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "<leader>w", ':bw<CR>')
-vim.keymap.set("n", "<leader>1", ':BufferLineGoToBuffer 1<CR>')
-vim.keymap.set("n", "<leader>2", ':BufferLineGoToBuffer 2<CR>')
-vim.keymap.set("n", "<leader>3", ':BufferLineGoToBuffer 3<CR>')
-vim.keymap.set("n", "<leader>4", ':BufferLineGoToBuffer 4<CR>')
-vim.keymap.set("n", "<leader>5", ':BufferLineGoToBuffer 5<CR>')
+vim.keymap.set("n", "<leader>w", ":bw<CR>")
+vim.keymap.set("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>")
+vim.keymap.set("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>")
+vim.keymap.set("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>")
+vim.keymap.set("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>")
+vim.keymap.set("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>")
 
 -- shared keys
-vim.keymap.set('n', ';', ':')
-vim.keymap.set('i', 'jj', '<ESC>')
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("i", "jj", "<ESC>")
+
+-- Get relative file path
+vim.keymap.set(
+  "n",
+  "cp",
+  ":let @a = substitute(expand('%:p'), '.*\\/src', 'src', '') .. ':' .. line('.') | call system('xclip -selection clipboard', @a)<CR>",
+  { noremap = true, silent = true, desc = "Get relative file path" }
+)
