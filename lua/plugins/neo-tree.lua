276bede137ad4fe0e793d7c25d455aa57151a4c2
Andrey Davidov <88237046+m4a1j9@users.noreply.github.com>
2024-08-14 14:52:55 +0700

    neo-tree & gitsigns fix

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,42 +1,58 @@
 local M = {}
 
 M.plugin = {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    M.setup()
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		M.setup()
+	end,
 }
 
 M.setup = function()
-  require("neo-tree").setup({
-    window = {
-      width = 30,
-    },
-    filesystem = {
-      filtered_items = {
-        never_show_by_pattern = {
-          ".git",
-          ".idea",
-          ".vscode",
-        },
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-    },
-  })
-  -- Открыть-закрыть файловый менеджер
-  vim.keymap.set("n", "<A-1>", ":Neotree filesystem reveal left<CR>", {})
+	require("neo-tree").setup({
+		window = {
+			width = 35,
+		},
+		filesystem = {
+			filtered_items = {
+				never_show_by_pattern = {
+					".git",
+					".idea",
+					".vscode",
+				},
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+		},
+		default_component_configs = {
+			indent = {
+				indent_size = 1,
+			},
+		},
+	})
+	-- Открыть-закрыть файловый менеджер
+	vim.keymap.set(
+		"n",
+		"<A-1>",
+		":Neotree filesystem reveal left<CR>",
+		{ noremap = true, silent = true, desc = "Neotree reveal" }
+	)
+	vim.keymap.set(
+		"n",
+		"<A-1>1",
+		":Neotree filesystem toggle left<CR>",
+		{ noremap = true, silent = true, desc = "Neotree toggle" }
+	)
 end
 
 if not pcall(debug.getlocal, 4, 1) then
-  M.setup()
+	M.setup()
 end
 
 return M
