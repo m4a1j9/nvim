d768760cc2c61fec27f79f6c5b1290ce74f987f6
Andrey Davidov <88237046+m4a1j9@users.noreply.github.com>
2024-09-12 11:40:13 +0700

    mappings refactor

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,6 +10,8 @@ local opts = {
 	silent = true,
 }
 
+--===== native =====--
+
 -- Move thorugh windows --
 
 opts.desc = "Go to the left window"
@@ -24,7 +26,56 @@ vim.keymap.set("n", "<A-k>", ":wincmd k<CR>", opts)
 opts.desc = "Go to the right window"
 vim.keymap.set("n", "<A-l>", ":wincmd l<CR>", opts)
 
--- dap --
+-- shared keys --
+
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("i", "jj", "<ESC>")
+opts.desc = "Get relative file path"
+vim.keymap.set(
+	"n",
+	"cp",
+	":let @a = substitute(expand('%:p'), '.*\\/src', 'src', '') .. ':' .. line('.') | call system('xclip -selection clipboard', @a)<CR>",
+	opts
+)
+
+-- Search highlighted --
+
+opts.desc = "Search for highlighted text in current buffer"
+vim.keymap.set("v", "/s", 'y/<C-R>"<CR>', opts)
+
+opts.desc = "Search for exact highlighted word"
+vim.keymap.set("v", "/w", 'y/\\<<C-R>"\\><CR>', opts)
+
+-- Show off search highlight
+
+opts.desc = "Show off search highlight"
+vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>", opts)
+
+-- mass replace shortcut
+
+opts.desc = vim.keymap.set("v", "/rr", 'y:%s/<C-R>"//g<Left><Left>', { noremap = true, desc = "Mass replace" })
+vim.keymap.set("v", "/rc", 'y:%s/<C-R>"//gc<Left><Left><Left>', { noremap = true, desc = "Mass replace with asking" })
+vim.keymap.set(
+	"v",
+	"/ri",
+	'y:%s/<C-R>"//gi<Left><Left><Left>',
+	{ noremap = true, desc = "Mass replace with case sentetive" }
+)
+vim.keymap.set(
+	"v",
+	"/ra",
+	'y:%s/<C-R>"//gci<Left><Left><Left><Left>',
+	{ noremap = true, desc = "Mass replace with case and asking" }
+)
+-- g => global search
+-- c => Ask for confirmation first
+-- i => Case insensitive
+
+--===== native end =====--
+
+--===== plugins =====--
+
+-- DAP --
 
 opts.desc = "DAP continue"
 vim.keymap.set("n", "<F5>", require("dap").continue, opts)
@@ -62,7 +113,7 @@ end)
 opts.desc = "DAP toggle ui"
 vim.keymap.set("n", "<leader>ui", require("dapui").toggle)
 
--- bufferline --
+-- Bufferline --
 
 local close_buffer = function()
 	vim.cmd("bd")
@@ -96,34 +147,7 @@ vim.keymap.set("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", opts)
 opts.desc = "Close others buffers"
 vim.keymap.set("n", "<leader>ww", ":BufferLineCloseOthers<CR>", opts)
 
--- shared keys
-vim.keymap.set("n", ";", ":")
-vim.keymap.set("i", "jj", "<ESC>")
-
--- Get relative file path
-
-opts.desc = "Get relative file path"
-vim.keymap.set(
-	"n",
-	"cp",
-	":let @a = substitute(expand('%:p'), '.*\\/src', 'src', '') .. ':' .. line('.') | call system('xclip -selection clipboard', @a)<CR>",
-	opts
-)
-
--- Search highlighted
-
-opts.desc = "Search for highlighted text in current buffer"
-vim.keymap.set("v", "/s", 'y/<C-R>"<CR>', opts)
-
-opts.desc = "Search for exact highlighted word"
-vim.keymap.set("v", "/w", 'y/\\<<C-R>"\\><CR>', opts)
-
--- Show off search highlight
-
-opts.desc = "Show off search highlight"
-vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>", opts)
-
--- Telescope in specific dirrectory
+-- Telescope in specific dirrectory --
 
 opts.desc = "Search in specific dir"
 vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", opts)
@@ -131,7 +155,7 @@ vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", opts)
 opts.desc = "Find files in specific dir"
 vim.keymap.set("n", "<leader>pd", "<cmd>Telescope dir find_files<CR>", opts)
 
--- Telescope
+-- Telescope --
 
 local builtin = require("telescope.builtin")
 
@@ -147,6 +171,12 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, opts)
 opts.desc = "Find tags"
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
 
--- Neogit
+opts.desc = "Recent files"
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, opts)
+
+-- Neogit --
+
 opts.desc = "Open Neogit"
 vim.keymap.set("n", "<leader>no", ":Neogit<CR>", opts)
+
+--===== plugins end =====--
