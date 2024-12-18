d7f5ea4fa05be1b6f3992492ee29e3f1b5e04a71
m4a1j9 <davidovandre5@gmail.com>
2024-12-18 15:02:28 +0700

    neogit diffview

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -1,13 +1,22 @@
 local M = {}
 
 M.plugin = {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-  },
-  config = true
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+	},
+	config = function ()
+	  M.setup()
+	end,
 }
 
-return M
+M.setup = function()
+	require("neogit").setup({
+		integrations = {
+			diffview = true,
+		},
+	})
+end
 
+return M
